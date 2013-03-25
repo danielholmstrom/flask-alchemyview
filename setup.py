eb6f39877f0687b0b21868ad0d4aeb4fd4f9b47c
Daniel Holmstrom <holmstrom.daniel@gmail.com>
2013-03-25 12:51:04 +0100

    Changed version to 0.1.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requires = install_requires + [
 ]
 
 setup(name='Flask-AlchemyView',
-      version='0.1',
+      version='0.1.1',
       description="Simple ModelView for auto-generating Flask Views based on "
       "SQLAlchemy models",
       long_description=README,
